0c1b4d9ad6f70bc59412bd1dcd77137736a9a7e4
Pedro Inácio Penha dos Santos <123902574+pedroinaciop@users.noreply.github.com>
2025-11-09 22:17:22 -0300

    CORS atualizado

--- src/main/java/com/monitoramento/saude/security/SecurityConfiguration.java
+++ src/main/java/com/monitoramento/saude/security/SecurityConfiguration.java
@@ -58,7 +58,7 @@ public class SecurityConfiguration {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(List.of(
                 "http://localhost:3000/",
-                "http://localhost:3001/",
+                "https://monitoramento-alimentar.vercel.app/",
                 "https://adminpainel.store/"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
